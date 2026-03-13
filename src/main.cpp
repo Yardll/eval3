2aa0ed501687672348ae953690347cb7570e28de
Yardll <ybelkadi025@gmail.com>
2026-03-13 11:02:47 +0000

    deuxieme commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,27 @@ using namespace std;
 
 void afficherBornes(const float puiss[], const float dist[], int taille);
 int indiceBorneLaPlusProche(const float dist[], int taille);
+float calculerTempsRecharge(float capacite, float puissance);
 
 
 int main() {
-    float puissance[5] = {50.0, 22.0, 11.0, 150.0, 7.4}; //en kW
-    float distances[5] = {12.5, 35.0, 8.0, 60.0, 2.3}; //en Km
+    float puissance[5] = {50.0, 22.0, 11.0, 150.0, 7.4};//en kW
+    float distances[5] = {12.5, 35.0, 8.0, 60.0, 2.3};// en Km
 
-    for(int i = 0; i < 5; i++) {
-        cout << "Borne " << i+1 << " : " << puissance[i] << "Kw, à " << distances[i] << " km \n";
-    }
+    afficherBornes(puissance, distances, 5);
+
+    int indice = indiceBorneLaPlusProche(distances, 5);
+    cout << "Indice de la borne la plus proche : " << indice << "!\n";
 
     return 0;
-    
 }
+
 void afficherBornes(const float puiss[], const float dist[], int taille)
 {
     for(int i = 0; i < taille; i++)
-        cout << "Borne " << i + 1 << " : " << puiss[i] << " kW " << dist[i] << " km" << endl;
+        cout << "Borne " << i + 1 << " : " << puiss[i] << " kW " << dist[i] << " km" << "!\n";
 }
+
 int indiceBorneLaPlusProche(const float dist[], int taille)
 {
     int indice = 0;
@@ -29,4 +32,3 @@ int indiceBorneLaPlusProche(const float dist[], int taille)
             indice = i;
     return indice;
 }
-    
